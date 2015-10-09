fd07ec47e2e03f89f83b2b3df31262ebb2ccd6a7
Cary Small <Yepster1@live.co.za>
2015-10-09 20:14:40 +0200

    added fixed div

--- Starter.js
+++ Starter.js
@@ -1,6 +1,5 @@
-var height = document.body.clientHeight;
-var width = document.body.clientWidth;
-var svg = d3.select("body").append("svg")
-								.attr("width",width)
-								.attr("height",5000)
-svg.append("rect").attr("width",width).attr("height",500).attr("fill","red");
+var height = $(window).height();
+var width = $(window).width();
+
+
+//%(".MainSquare").css({left:0,top:100});
