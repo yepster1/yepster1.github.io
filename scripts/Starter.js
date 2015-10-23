e83bd450aaddbabf255ebda90c30f211d4371830
Yepster <CarySmall2295@gmail.com>
2015-10-23 20:54:03 +0200

    Using midpoint circle algarythm

--- scripts/Starter.js
+++ scripts/Starter.js
@@ -19,40 +19,72 @@ function toRGB (color){
     		return parseInt(x); 
 		});
 }
-function colorChanger(x,y,sign){
+function colorChanger(x,y,sign,amount){
 	var currentColor  = rectangles[x][y].attr("fill");
-	//var position = rectangles[x][y].id.split(",");
 	var colors = toRGB(currentColor);
 	var k = colors[0];
 	var l = colors[1];
 	if(sign == '+'){
-		k = colors[0]+10;
-		l = colors[1]+10;
+		k = colors[0]+amount;
+		l = colors[1]+amount;
 	}else{
-		k = colors[0]-10;
-		l = colors[1]-10;	
+		k = colors[0]-amount;
+		l = colors[1]-amount;	
 	}
 
-	var newColor = "rgb("+k+","+l+","+Math.floor((l+k)/2)+")";	return newColor;
-	//debugger;
-	return "red";
+	var newColor = "rgb("+k+","+l+","+Math.floor((l+k)/2)+")";	return newColor;;
 }
 function currentSquare(x,y,width,height,sign){
 	for (var i = x-width; i <= x+width; ++i) {
-		rectangles[y-height-1][i].attr("fill",colorChanger(y-height-1,i,sign));
-		rectangles[y+height][i].attr("fill",colorChanger(y+height,i,sign));
+		rectangles[y-height-1][i].attr("fill",colorChanger(y-height-1,i,sign,10));
+		rectangles[y+height][i].attr("fill",colorChanger(y+height,i,sign,10));	
 	};
 	for (var i = y-height; i < y+height; ++i) {
-		rectangles[i][x-width].attr("fill",colorChanger(i,x-width,sign));
-		rectangles[i][x+width].attr("fill",colorChanger(i,x+width,sign));
+		rectangles[i][x-width].attr("fill",colorChanger(i,x-width,sign,10));
+		rectangles[i][x+width].attr("fill",colorChanger(i,x+width,sign,10));
 	};
 }			
 function rippleEffect(x,y){
-	for(var k = 0; k < 10; ++k){
+	/*for(var k = 0; k < 10; ++k){
 		currentSquare(x,y,k,k,'+');
 		currentSquare(x,y,k-2,k-2,'-');
-	}
+	}*/
+//	for(var i = 0; i < 15; i++){
+		currentCircle(x,y,3,'+',2);
+//	}
 }
+function calculateDistance(x,y,cx,cy,radius,Limit){
+	var distance = Math.sqrt(Math.pow(y-cy,2) + Math.pow(x-cx,2)); //Distance formula
+	if(Limit > 100){return -1};
+	if(distance > radius){
+		return calculateDistance(x,y,cx,--cy,radius,++Limit); 
+	}
+	return cy;
+}
+
+function currentCircle(x0,y0,radius,sign,amount){
+	//MidPoint_Circle_Algarithm
+	var x = radius;
+	var y = 0;
+	var decisionOver2 = 1-x;
+	while(x >= y){
+		rectangles[x+x0][y+y0].attr("fill",colorChanger(x+x0,x+y0,sign,10));
+		rectangles[y+x0][x+y0].attr("fill",colorChanger(y+x0,x+x0,sign,10));
+		rectangles[-x+x0][y+y0].attr("fill",colorChanger(-x+x0,x+y0,sign,10));
+		rectangles[-y+x0][x+y0].attr("fill",colorChanger(-y+x0,x+x0,sign,10));
+		rectangles[-x+x0][y+y0].attr("fill",colorChanger(-x+x0,-x+y0,sign,10));
+		rectangles[-y+x0][x+y0].attr("fill",colorChanger(-y+x0,-x+x0,sign,10));
+		rectangles[x+x0][y+y0].attr("fill",colorChanger(x+x0,-x+y0,sign,10));
+		rectangles[y+x0][x+y0].attr("fill",colorChanger(y+x0,-x+x0,sign,10));
+	y++;
+	if(decisionOver2 <0){
+		decisionOver2 += 2 * y + 1;
+	}else{
+		x--;
+		decisionOver2 += 2 * (y - x) +1;
+	}
+	}
+} 
 
 var squarerectangles = {width: 10, height: 10};
 var x = 0;
@@ -60,14 +92,13 @@ var y = 0;
 for (var i = 0; i < width; i = i + squarerectangles.width){
 	var row = [];
 	for (var k = 0; k < height; k = k + squarerectangles.height){
-		row.push(d3.select(".canvas").append("rect")
+		row.push(d3.select(".canvas").append("circle")
 			.attr("fill",function(){
 				return "rgb("+Math.floor(colorW(i))+","+Math.floor(colorH(k))+","+Math.floor(colorB((i+k)))+")";
 			})
-			.attr("x",i)
-			.attr("y",k)
-			.attr("width",squarerectangles.width)
-			.attr("height",squarerectangles.height)
+			.attr("cx",i)
+			.attr("cy",k)
+			.attr("r",squarerectangles.width)
 			.attr("id",x+++","+y)
 			.on("click",function(d){
 				var position = this.id.split(",");
